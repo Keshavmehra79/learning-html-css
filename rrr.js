ba58818385d43bc06d73adb32df3f87ef99b5399
keshavmehra79 <mehrakeshav332@gmail.com>
2025-06-14 17:53:45 +0530

    14june

---
+++ rrr.js
@@ -0,0 +1,6 @@
+// var r=4
+// console.log(3.14*4*4)
+//BY DEFAULT INPUT STRING TYPE LETA HAI WITHOUT PARSEINT
+var a=parseInt(prompt("Enter first number: "))
+var b=parseInt(prompt("Enter sec nnumber: "))
+console.log(a+b)
